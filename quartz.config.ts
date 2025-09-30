3d5bbd28ff20d2c0182990d8528f11e951167228
DaveParkinglot <159180002+DaveParkinglot@users.noreply.github.com>
2025-09-30 10:45:02 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -76,6 +76,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+	  Plugin.LeafletMap(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
