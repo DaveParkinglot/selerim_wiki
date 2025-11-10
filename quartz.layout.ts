14dad935b0b4502efb75ef1f06ca815b4f5ca683
DaveParkinglot <159180002+DaveParkinglot@users.noreply.github.com>
2025-11-10 13:10:37 -0500

    new

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "GitHub Repo": "https://github.com/DaveParkinglot/selerim_wiki",
+      "D&D Discord Community": "https://discord.gg/2HJh5jbmYw",
     },
   }),
 }
