2179522bdf64e83ff0020971735a23eb2c60d205
DaveParkinglot <159180002+DaveParkinglot@users.noreply.github.com>
2025-12-24 08:31:58 -0500

    Update 404.tsx

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,4 +1,8 @@
+import { i18n } from "../../i18n"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
+
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
+  // If baseUrl contains a pathname after the domain, use this as the home link
   const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
   const baseDir = url.pathname
 
@@ -6,11 +10,10 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
-
-      {/* 👇 Your custom line */}
       <p>This page has yet to be written...</p>
-
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
 }
+
+export default (() => NotFound) satisfies QuartzComponentConstructor
