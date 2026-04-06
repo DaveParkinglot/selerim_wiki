26dd9bd97c66f78c553e3871c39547fe0d97aa24
DaveParkinglot <159180002+DaveParkinglot@users.noreply.github.com>
2026-04-06 14:03:52 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -95,6 +95,7 @@ const config: QuartzConfig = {
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
+	  Plugin.ClickableImages()
     ],
   },
 }
