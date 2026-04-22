f7b2425b2c7dda5c7a9cb8a797201d7f800c0556
Jordan <Jordan>
2026-04-22 08:45:58 -0400

    v5

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -9,7 +9,7 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   return (
     <article class="popover-hint">
       <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <p>This page has yet to be written...</p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
       <script
         dangerouslySetInnerHTML={{
