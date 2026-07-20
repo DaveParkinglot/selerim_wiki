d648eab94be614177cc5724d5382838e57c0a2ac
Jordan Nolte <jk.nolte@gmail.com>
2026-07-20 09:14:15 -0400

    style

--- quartz.ts
+++ quartz.ts
@@ -1,3 +1,9 @@
+// Sets Recent Notes component to only load on Index page //
+
+import { registerCondition } from "./quartz/plugins/loader/conditions"
+registerCondition("index-only", (props) => props.fileData.slug === "index")
+//
+
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
 const config = await loadQuartzConfig()
